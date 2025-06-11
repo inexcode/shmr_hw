83acfbb1bd496a1030296eec5c7cb165ebfdc855
Inex Code <gitea@inex.rocks>
2025-06-11 22:58:58 +0300

    feat: Add abstract repositories

---
+++ lib/logic/repositories/abstract/accounts.dart
@@ -0,0 +1,22 @@
+import 'package:shmr_hw/logic/models/account.dart';
+
+abstract class AccountsRepository {
+  Future<List<Account>> fetchAccounts();
+
+  Future<Account> createAccount({
+    required final AccountRequest request,
+  });
+
+  Future<AccountDetails> getAccount({
+    required final int id,
+  });
+
+  Future<Account> updateAccount({
+    required final int id,
+    required final AccountRequest request,
+  });
+
+  Future<AccountHistoryResponse> getAccountHistory({
+    required final int id,
+  });
+}
---
+++ lib/logic/repositories/abstract/transactions.dart
@@ -0,0 +1,26 @@
+import 'package:shmr_hw/logic/models/transaction.dart';
+
+abstract class TransactionsRepository {
+  Future<Transaction> createTransaction({
+    required final TransactionRequest request,
+  });
+
+  Future<TransactionResponse> getTransaction({
+    required final int id,
+  });
+
+  Future<TransactionResponse> updateTransaction({
+    required final int id,
+    required final TransactionRequest request,
+  });
+
+  Future<void> deleteTransaction({
+    required final int id,
+  });
+
+  Future<List<TransactionResponse>> fetchTransactions({
+    final int accountId,
+    final DateTime? startDate,
+    final DateTime? endDate,
+  });
+}
