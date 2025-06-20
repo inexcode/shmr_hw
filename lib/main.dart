bb80a7292b27ea52778eef75958b8787f7f129c4
Inex Code <inex.code@selfprivacy.org>
2025-06-20 18:19:33 +0300

    feat: REST API client

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shmr_hw/config/localization.dart';
 import 'package:shmr_hw/ui/router/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
 
   await EasyLocalization.ensureInitialized();
 
